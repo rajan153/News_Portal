c294ea08ee5204ec10cf491e835f35723413eca2
Rajan <rajanmasih154@gmail.com>
2024-06-20 00:28:44 +0530

    style: Format Navbar component code for readability

--- src/components/Common/Navbar.jsx
+++ src/components/Common/Navbar.jsx
@@ -3,7 +3,9 @@ import React from "react";
 function Navbar() {
   return (
     <div>
-      <h1 className="bg-[#0B594E] text-[#d3e9e6] p-4 font-bold text-2xl text-center">React News Portal</h1>
+      <h1 className="bg-[#0B594E] text-[#d3e9e6] p-4 font-bold text-2xl text-center">
+        React News Portal
+      </h1>
     </div>
   );
 }
