5673f76af3fb9965c7a58566d518e1ceee2048ca
Rajan <rajanmasih154@gmail.com>
2024-06-20 00:28:30 +0530

    Add Home page component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import { useEffect, useState } from "react";
+import { fetchNews } from "./hooks/useNewsData";
+import Navbar from "./components/Common/Navbar";
 
 function App() {
+  const [searchKey, setSearchKey] = useState("");
+  const [news, setNews] = useState("");
+  const [loading, setLoading] = useState("");
 
+  async function searchForNews(category = "All-News", searchKey = "") {
+    setLoading(true);
+    const response = await fetchNews(category, searchKey);
+    setNews(response.data);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    // searchForNews();
+  }, []);
   return (
     <div>
-      <h1 className="bg-black text-white">React News Portal</h1>
+      <Navbar />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
