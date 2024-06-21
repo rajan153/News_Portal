62b5d1c0f5e991256845c5d1d1143450721e43ac
Rajan <rajanmasih154@gmail.com>
2024-06-21 14:40:29 +0530

    chore: Update App component to use react-router-dom Outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,11 @@
-import { useEffect, useState } from "react";
-import { fetchNews } from "./hooks/useNewsData";
+import { Outlet } from "react-router-dom";
 import Navbar from "./components/Common/Navbar";
 
 function App() {
-  const [searchKey, setSearchKey] = useState("");
-  const [news, setNews] = useState("");
-  const [loading, setLoading] = useState("");
-
-  async function searchForNews(category = "All-News", searchKey = "") {
-    setLoading(true);
-    const response = await fetchNews(category, searchKey);
-    setNews(response.data);
-    setLoading(false);
-  }
-
-  useEffect(() => {
-    // searchForNews();
-  }, []);
   return (
     <div>
       <Navbar />
+      <Outlet />
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import store from "./store/store.js";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import DetailNews from "./Pages/DetailNews.jsx";
+import Home from "./Pages/Home.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/" element={<Home />} />
+      <Route path="/news/:id" element={<DetailNews />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
+);
