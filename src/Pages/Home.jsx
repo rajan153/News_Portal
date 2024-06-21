ad98597fc67a1ee1fb35f797d7176164e138506b
Rajan <rajanmasih154@gmail.com>
2024-06-21 14:41:33 +0530

    feat: Improve Home page with lazy loading for images and loading state handling

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import Cards from "../components/Cards/Cards";
+import { useSelector } from "react-redux";
+import Footer from "../components/Common/Footer";
 
 function Home() {
+  const { data, totalPages, loading } = useSelector((state) => state.newsData);
+
   return (
-    <div>Home</div>
-  )
+    <div>
+      {loading ? (
+        <div className="flex items-center justify-center h-screen">
+          <p className="text-2xl">Loading...</p>
+        </div>
+      ) : (
+        <div>
+          {data.length === 0 ? (
+            <h1 className="text-center text-2xl font-bold mt-4">
+              No data found
+            </h1>
+          ) : (
+            <>
+              <Cards data={data} />
+              <Footer totalPages={totalPages} />
+            </>
+          )}
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default Home
+export default Home;
